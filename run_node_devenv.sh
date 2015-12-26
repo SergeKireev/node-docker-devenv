505617dc1226324e85397fb24730e61b09d18b10
sergei <sergei.kireev@hotmail.fr>
2015-12-26 19:16:52 +0100

    updated local ssh port

--- run_node_devenv.sh
+++ run_node_devenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOCAL_PORT=6002
+LOCAL_PORT=6000
 CONTAINER_PORT=22
 HOST_WEB_PORT=8080
 CONTAINER_WEB_SERVER_PORT=5000
@@ -14,7 +14,6 @@ NODE_GIT_REPO=$1
 
 if [ "$1" != "" ]
 then
-
     echo "Launching Node devenv\n"
     docker run -it --privileged -d -p ${LOCAL_PORT}:${CONTAINER_PORT} -p ${HOST_WEB_PORT}:${CONTAINER_WEB_SERVER_PORT} --name ${DOCKER_NAME} -v /net:/net -v ${RVIEWR_GIT_REPO}:/repo -v /dev:/dev ${DOCKER_IMAGE_NAME} 
 
